c9a0ceeb066eb74d4c65d67508f05956fc0bed64
Koteykee <koteykeethecat@gmail.com>
2026-03-24 15:13:06 +0300

    Footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import Logo from "../assets/Pizza-logo.png";
 
 export const Footer = () => {
   return (
-    <footer className="w-full p-10 border-t border-[#e2e2e2]">
+    <footer className="w-full bg-white p-8 border-t border-[#e2e2e2]">
       <div className="container h-18 flex flex-col items-center justify-center">
-        <img src={Logo} alt="Pizza Logo" className="w-20 rounded-full" />
-        <p>Pizza Slice</p>
+        <img src={Logo} alt="Pizza Logo" className="w-16 rounded-full" />
+        <p className="text-[24px] font-semibold">Pizza Slice</p>
         <p className="text-[#8f8f8f]">Best pizza in the world!</p>
       </div>
     </footer>
